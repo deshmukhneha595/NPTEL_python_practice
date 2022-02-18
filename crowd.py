56801608575931623f7df1908619d8aa305fdd4a
Neha  Deshmukh <deshmukhneha638@gmail.com>
2022-02-17 20:38:41 -1200

    Add files via upload

---
+++ crowd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 18 14:01:39 2022
+
+@author: Lenovo
+"""
+
+import matplotlib.pyplot as plt
+Estimates=[100,200,300,400,500,600,700]
+y=[]
+for i in range(len(Estimates)):
+    y.append(5)
+plt.plot(Estimates,y,'r--')
